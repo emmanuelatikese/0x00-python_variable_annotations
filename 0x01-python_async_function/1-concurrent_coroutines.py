191d6bc8ad28b79ef9fcff1a32b8d94a0daeca89
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-06-11 00:36:52 +0100

    adding all files

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''multiple concurrent'''
 from typing import List
+from asyncio import as_completed, create_task
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
@@ -8,5 +9,5 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     '''this is all about concurrent'''
     res = []
     for _ in range(n):
-        res.append(await wait_random(max_delay))
-    return sorted(res)
+        res.append(create_task(wait_random(max_delay)))
+    return [await x for x in as_completed(res)]
