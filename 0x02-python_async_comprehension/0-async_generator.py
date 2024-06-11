4c2f36c4f220b698b9ad4d4d91c3fb850506931f
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-06-11 23:23:24 +0100

    adding all files

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 '''this is a gen func'''
 import asyncio
 from random import uniform
+from typing import Generator
 
 
-async def async_generator() -> None:
+async def async_generator() -> Generator[float, None, None]:
     '''async function starts here'''
     for _ in range(10):
         await asyncio.sleep(1)
