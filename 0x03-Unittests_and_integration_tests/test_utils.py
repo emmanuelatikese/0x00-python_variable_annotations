ebd46ce87a199d42be42c9e43c480c7690553c4a
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-01 09:52:57 +0100

    file test_utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,11 +10,12 @@ from utils import access_nested_map
 class TestAccessNestedMap(unittest.TestCase):
     '''Function starts with parameterized.expand'''
     @parameterized.expand([
-        ("test_1", {"a": 1}, ("a",), 1),
-        ("test_2", {"a": {"b": 2}}, ("a",), {"b": 2}),
-        ("test_3", {"a": {"b": 2}}, ("a", "b"), 2)
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, name, nested_map: Mapping, path: Sequence, expected: Any) -> assert_equal:
+    def test_access_nested_map(self, nested_map: Mapping,
+                               path: Sequence, expected: Any):
         '''Checks access_nested_map function'''
         assert_equal(access_nested_map(nested_map, path), expected)
 
