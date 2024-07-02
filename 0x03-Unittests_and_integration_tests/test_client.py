ac7804b0b11b8d3f9d70d7beec0acb7291617c80
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-02 05:28:11 +0100

    file test_client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -44,6 +44,15 @@ class TestGithubOrgClient(unittest.TestCase):
             repo = test_obj.public_repos()
             self.assertEqual(repo, ["abc", "google"])
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        '''checking for license'''
+        res = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(res, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
