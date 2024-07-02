c96d5db08c38224241a5976ea43c32fa4ad2ba27
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-02 04:11:06 +0100

    file test_client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,7 +5,7 @@ import unittest
 from typing import Sequence, Mapping, Any
 from nose.tools import assert_equal
 from parameterized import parameterized
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -21,6 +21,16 @@ class TestGithubOrgClient(unittest.TestCase):
         test_obj = GithubOrgClient(org_name)
         test_obj.org()
         mock.assert_called_once_with(test_url)
+
+    def test_public_repos_url(self):
+        '''testing public_repos in client.py'''
+        with patch("client.GithubOrgClient.org",
+                   new_callable=PropertyMock) as mk:
+            mk.return_value = {
+                "repos_url": "https://api.github.com/users/abc/repos"
+                }
+            test_obj = GithubOrgClient("abc")._public_repos_url
+            self.assertEqual(test_obj, mk.return_value["repos_url"])
 
 
 if __name__ == '__main__':
